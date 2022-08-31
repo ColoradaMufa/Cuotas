a56dfa169841c9f5aca516a306a739cfc0c9dce6
ColoradaMufa <maycafragola@gmail.com>
2022-08-31 18:15:44 -0300

    Proyecto Final

--- cuotas.js
+++ cuotas.js
@@ -1,49 +1,51 @@
-let producto = prompt ("Ingrese el producto");
-let  precio_producto = parseInt(prompt("Ingrese el costo de " + producto));
-let cantidad_de_productos = parseInt(
-    prompt ("Indique la cantidad que desea comprar")
-);
-let cuota_producto = parseInt(
-    prompt("Indique la cantidad de cuotas, 3/6/12")
-);
-
-function precio_total(precio_producto, cantidad_de_productos) {
-    let costo = precio_producto * cantidad_de_productos;
-    return costo;
-}
-
-function precio_en_cuotas(precio_producto, cantidad_de_productos){
-    let porcentaje_en_cuotas =
-        (precio_total(precio_producto, cantidad_de_productos));
-    let precio_final =
-        precio_total(precio_producto, cantidad_de_productos) + porcentaje_en_cuotas;
-
-    if(cuota_producto == 3) {
-        return (
-            "El precio final de " +
-            producto +
-            "es de $" +
-            precio_final * 1.15
-        );
+let objeto = [
+    {
+        nombre: "moños",
+        precio: 450,
+        cantidad: 0,
+        cuotas: '',
+        total: '',
+    },
+    {
+        nombre: "buzo",
+        precio: 2350,
+        cantidad: 0,
+        cuotas: '',
+        total: '',
+    },
+    {
+        nombre: "ruana",
+        precio: 1700,
+        cantidad: 0,
+        cuotas: '',
+        total: '',
     }
-    else if(cuota_producto == 6 ) {
-        return (
-            "El precio final de " +
-            producto +
-            " es de $" +
-            precio_final * 1.30
-        );
-    }
-    else if (cuota_producto == 12 ) {
-        return (
-        "El precio final de " +
-        producto +
-        " es de $" +
-        precio_final * 1.60
-        );
+]
+
+let producto = prompt(`Ingrese el producto - Moño - Buzo Ruana`);
+for (let i = 0; i < objeto.length; i++) {
+    if (objeto[i].nombre == producto) {
+        let cantidad_de_productos = parseInt(prompt("Indique la cantidad que desea comprar"));
+        objeto[i].cantidad = cantidad_de_productos;
+        let cuota_producto = parseInt(prompt("Indique la cantidad de cuotas, 3/6/12"));
+        if (cuota_producto == 0) {
+            alert(precio_total(objeto[i].precio, objeto[i].cantidad));
+        } else {
+            precio_cuotas(objeto[i].cantidad, objeto[i].precio, cuota_producto, objeto[i].nombre)
+        }
     }
 }
 
-console.log(precio_en_cuotas(precio_producto, cantidad_de_productos));
+function precio_total(precio, cantidad) {
+    return precio * cantidad;
+}
 
-console.log(precio_total(precio_producto, cantidad_de_productos))
+function precio_cuotas(cantidad, precio, cuota, nombre) {
+    if (cuota == 3) {
+        return (alert("El precio final de " + nombre + " es de $ " + ((cantidad * precio) * 1.15) / cuota))
+    } else if (cuota == 6) {
+        return (alert("El precio final de " + nombre + " es de $ " + ((cantidad * precio) * 1.30) / cuota))
+    } else if (cuota == 12) {
+        return (alert("El precio final de " + nombre + " es de $ " + ((cantidad * precio) * 1.60) / cuota))
+    }
+}

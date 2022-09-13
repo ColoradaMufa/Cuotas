34901d5ff87be66f330d2bd9aa80199385d08ef2
ColoradaMufa <maycafragola@gmail.com>
2022-09-12 21:26:07 -0300

    proyecto

--- cuotas.js
+++ cuotas.js
@@ -1,101 +1,97 @@
-
-
-const recuadro = document.createElement('img')
-image.src  = imagen.recuadro;
-const app=new Vue({
-    el: '#app',
-    data: {
-        prodAll: [
-            {   producto: {
-                Nombre: "Ruana",
-                Id: 0,
-                Stock: 5,
-                Precio: 2350,
-                url,
-                mensaje:"Hay stock",
-                alertaProd: "alertaOk",
-                //Para reseteo
-                StockInicial: 5,
-                mensajeInicial:"Hay stock",
-                alertaInicial: "alertaOk"
-            }},
-            {producto: {
-                Nombre: "Buzo",
-                Id: 1,
-                Stock: 10,
-                Precio: 1700,
-                imagesrc: "/pic",
-                mensaje:"Hay stock",
-                alertaProd: "alertaOk",
-                //Para reseteo
-                StockInicial: 10,
-                mensajeInicial:"Hay stock",
-                alertaInicial: "alertaOk"
-            }},
-            {producto: {
-                Nombre: "Moño",
-                Id: 2,
-                Stock: 3,
-                Precio: 450,
-                Url: "",
-                mensaje:"Hay stock",
-                alertaProd: "alertaOk",
-                //Para reseteo
-                StockInicial: 3,
-                mensajeInicial:"Hay stock",
-                alertaInicial: "alertaOk"
-            }}
-        ],
-        unidad: 1,
-        sumaUnidades: 0,
-        sumaPrecios: 0,
-        carrito:[]
+const productos = [
+    {
+        id: 1,
+        nombre: "Buzos",
+        precio: 2350,
+        cantidad: 1,
+        imagen: "/img/buzo4.jpg",
+        cuotas: '',
+        total: '',
     },
-    computed: {
-        listaCarrito() {
-            let salida='';
-                for (productoSel of this.carrito) {
-                    salida += productoSel;
-                }
-                salida += '';
-                //console.log;  
-                return salida;
+    {
+        id: 2,
+        nombre: "Ruana",
+        precio: 1700,
+        cantidad: 1,
+        imagen: "/img/RUANA5.jpeg",
+        cuotas: '',
+        total: '',
+    },
+    {
+        id: 3,
+        nombre: "Moño",
+        precio: 450,
+        cantidad: 1,
+        imagen: "/img/MONIO6.jpeg",
+        cuotas: '',
+        total: '',
+    }
+]
+//------------------------Declaracion de array------------------------
+let carrito = [];
+//------------------------Llamado para cargar los productos-----------
+mostrarProductos(productos);
+//--------------------------Selecciones-------------------------------
+const muestraCarrito = document.querySelectorAll('button#anadirAlCarritoId')
+
+// ------------------------ Eventos------------------------------------
+
+for (const muestra of muestraCarrito) {
+    muestra.addEventListener("click", mostrandoCarrito);
+}
+
+//------------------------- Funcionalidades----------------------------
+
+function mostrarProductos(productos) {
+    productos.forEach((producto) => {
+        let div = ` <div class="producto_general">
+        <h3>${producto.nombre}</h3>
+            <img class="" src="${producto.imagen}"" alt="">
+            <div>
+                <button onclick="agregarAlCarrito(${producto.id})" class="anadirAlCarrito" id="anadirAlCarritoId">Añadir</button>
+                <p>$ ${producto.precio}</p>
+            </div>
+        </div>
+    `
+        contenedorProductos.innerHTML += div;
+    });
+}
+
+function agregarAlCarrito(itemId) {
+    const prod = productos.find((prod) => prod.id === itemId)
+    carrito.length === 0 ? carrito.push(prod) : null;
+    let cont = 0;
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].id == prod.id) {
+            cont = cont + 1;
         }
-    },
-    methods: {
-        agregaProd(evento){
-            console.log('Id es ' + evento.target.id);
-            let target = evento.target.id
-            let producto = this.prodAll[target].producto
-            
-            if(producto.Stock>0) { 
-                producto.Stock -= 1;
-                this.sumaUnidades += 1;
-                this.sumaPrecios += producto.Precio;
-
-                let = elemento= {
-                nombre: producto.Nombre,
-                cantidad:this.unidad,
-                valor:"$ " + producto.Precio
-                }
-                this.carrito.push(elemento);
-            }
-            if (producto.Stock<=0) {
-                producto.mensaje="No hay stock disponible";
-                producto.alertaProd="alertaNok"
-            }
-            
-        },
-        limpiarCarrito(){
-            this.carrito=[];
-            this.sumaUnidades = 0;
-            this.sumaPrecios = 0;
-
-            for (item in this.prodAll) {
-                this.prodAll[item].producto.Stock = this.prodAll[item].producto.StockInicial;
-                this.prodAll[item].producto.mensaje = this.prodAll[item].producto.mensajeInicial;
-                this.prodAll[item].producto.alertaProd = this.prodAll[item].producto.alertaInicial;
+    }
+    if (cont == 0) {
+        carrito.push(prod)
+    } else {
+        for (let i = 0; i < carrito.length; i++) {
+            if (carrito[i].id == prod.id) {
+                carrito[i].cantidad += 1;
             }
         }
     }
-})
+}
+
+function mostrandoCarrito() {
+    carrito.forEach(carrito => {
+        let mostrar = `<p>${carrito.nombre}</p>
+        <p>$ ${carrito.precio} </p>
+        <div class="cantidad">
+        <button class="boton_incremento" id="boton_incremento">-</button>
+        <p class="" id="">${carrito.cantidad}</p>
+        <button class="boton_decremento" id="boton_decremento">+</button>
+        </div>`
+        muestra_carrito.innerHTML += mostrar;
+    })
+}
+
+
+function actualizarPrecioFinal() {
+    precioFinal.innertText = carrito.reduce((acc, producto) => acc += producto.precio * producto.cantidad, 0);
+
+};
